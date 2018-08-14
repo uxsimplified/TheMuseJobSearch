8bf62929c84cf5487bc8589f972eae9d7a7af358
juliehudspeth <38337033+juliehudspeth@users.noreply.github.com>
2018-08-13 20:50:11 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,24 +15,28 @@ $(document).ready(function () {
 
     $.getJSON(api, function(data) {
       console.log(data);
+      console.log (data.results[0].refs.landing_page);
+      console.log (data.results[0].name);
+      console.log(data.results[0].company.name);
+
 
       var queryLink = data.results[0].refs.landing_page;
       var queryName = data.results[0].name;
       var queryCompany = data.results[0].company.name;
-      var queryLocation = data.results[0].locations.name;
-
+ 
+    //Returns No result
+     if (data.results[0].length === 0) {
+        $(".search-result").append("<li class='no-result'><p>Oops, no results!</p></li>");
+      }
       //Iritating through arrays
-      for(var i = 0; i < data.length; i++) {
-        $(".search-result").append("<li> <a href = " + queryLink[i] + " target='_blank'>"+ queryName[i] + "</a> <p>" + queryCompany[i] + "</p>" + queryLocation[i] + "</p> </li>");
+      for(var i = 0; i < data.results[0].length; i++) {
+        $(".search-result").append("<li> <a href = " + queryLink[i] + " target='_blank'>" + queryName[i] + "</a> <p>" + queryCompany[i] + "</p> </li>");
       }
 
-      //Returns No result
-      if (data.results[i].length === 0) {
-        $(".search-result").append("<li class='no-result'><p>No results - try changing search options.</p></li>");
-      }
     })
+      
 
-    //Push heading up after clicing Search
+    //Push heading up after clicking Search
     $(".title").animate({"margin-top":"20px","margin-bottom":"20px"});
     //On smaller devices
     var mq = window.matchMedia("(min-width: 575px)");
@@ -41,7 +45,7 @@ $(document).ready(function () {
     }
   });
 
-  //Using Enter key for searching
+ //Using Enter key for searching
   $("#search-query").keypress(function (e){
       if (e.which === 13) {
           e.preventDefault();
